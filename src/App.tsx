8740a75a85da3ccd04433fcefca3e8606e50dd9b
Dolphin6116 <joshfaks@gmail.com>
2025-12-09 00:05:04 -0500

    Final fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Navbar } from "./components/Navbar";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <Navbar />
