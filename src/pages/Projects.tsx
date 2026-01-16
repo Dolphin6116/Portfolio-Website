1be96a97f1e85bb8b7d2e18f094cb48b637b1fb9
Dolphin6116 <joshfaks@gmail.com>
2026-01-15 19:19:36 -0500

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,8 +4,12 @@ const Projects = () => {
       <div className="text">Here are some projects I have worked on</div>
       <ul className="list">
         <h1 className="special">Projects</h1>
-        <li>- Calculator Game</li>
+        <li>- II-84 Calculator Program</li>
+        <li>- XAML studying program (Still in Progress)</li>
+        <li>- This Website!!!</li>
       </ul>
+      Check them out on my Github 
+      <a href ="https://github.com/Dolphin6116">https://github.com/Dolphin6116</a>
     </>
   );
 };
